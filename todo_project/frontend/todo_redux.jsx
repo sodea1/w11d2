dc0837ff2858191ceb1b12fcde1b58bb28c2fb3d
App Academy Student <email@email.com>
2022-04-12 11:57:00 -0400

    first

---
+++ todo_project/frontend/todo_redux.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const main = document.getElementById("main");
+  ReactDOM.render(<h1>Boo-Yah App</h1>, main);
+})
