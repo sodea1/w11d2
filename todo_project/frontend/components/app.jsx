351bdcfeff30b7951203b37f3b3a1a94bdd4eb55
App Academy Student <email@email.com>
2022-04-13 12:17:32 -0400

    fixed root store error

---
+++ todo_project/frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1>Fun</h1>
+  )
+}
+
+export default App;
--- todo_project/frontend/todo_redux.jsx
+++ todo_project/frontend/todo_redux.jsx
@@ -3,19 +3,20 @@ import ReactDOM from "react-dom";
 import { addTodos, addTodo, removeTodo } from "./actions/todo_actions";
 import configureStore from "./store/store";
 import * as steps from "./actions/step_actions";
+import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
   const main = document.getElementById("main");
-  ReactDOM.render(<h1>Boo-Yah App</h1>, main);
   const store = configureStore();
   window.store = store;
   window.addTodos = addTodos;
   window.addTodo = addTodo;
   window.removeTodo = removeTodo;
-
+  
   window.receiveSteps = steps.receiveSteps;
   window.receiveStep = steps.receiveStep;
   window.removeStep = steps.removeStep;
-
-
+  
+  ReactDOM.render(<Root store={store}/>, main);
+  
 })
